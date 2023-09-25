904f53a0458175a5f985d71fd8c8af43202516d9
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2023-09-25 22:43:19 +0530

    feat: setup eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+};
